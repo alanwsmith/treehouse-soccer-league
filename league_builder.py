eb327c3b67648005a1253b027ee1587805f7ee72
Alan W. Smith <alan@alanwsmith.com>
2016-12-27 22:21:51 -0500

    Added comment about relying on the specific input file for the split.

--- league_builder.py
+++ league_builder.py
@@ -9,6 +9,12 @@ import random
 # be a number of both experienced and inexperienced 
 # players that can be split evenly across teams. 
 
+# This script also relies on the fact that there are 18
+# players in the input file and that there are three 
+# teams. Because that works out to 6 players per team,
+# no additional calculation is done to keep the 
+# same number of players on each team.
+
 # Also, this currently stores players in a player_dict
 # with names for keys. If a new list with two players 
 # who have the same name is used, one of them will 
