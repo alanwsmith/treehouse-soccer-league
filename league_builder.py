4396cef937ba6079396137485a4eca6db3ec2115
Alan W. Smith <alan@alanwsmith.com>
2016-12-27 19:57:08 -0500

    Loading players into a list.

--- league_builder.py
+++ league_builder.py
@@ -1,3 +1,24 @@
+#!/usr/bin/env python 
+
+import csv
+
+
 if __name__ == "__main__":
-	  print("TKTKTKTK")
+	
+	players = []
+
+	def load_players(): 
+	
+	    with open('soccer_players.csv', 'r') as csv_file:
+	        csv_data = csv.DictReader(csv_file)
+	        for row in csv_data:
+	        	players.append({
+                    'name': row['Name'],
+                    'height': row['Height (inches)'],
+                    'experience': row['Soccer Experience'],
+                    'guardian': row['Guardian Name(s)']
+	        	})
+
+	load_players()
+	print(players)
 
