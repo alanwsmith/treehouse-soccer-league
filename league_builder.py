f48de0445539ff5a9f5f6f7768bde45010d6718a
Alan W. Smith <alan@alanwsmith.com>
2016-12-27 22:16:46 -0500

    Added comment about the fact that the script won't work if a new player list is used where two players have the same name.

--- league_builder.py
+++ league_builder.py
@@ -9,6 +9,13 @@ import random
 # be a number of both experienced and inexperienced 
 # players that can be split evenly across teams. 
 
+# Also, this currently stores players in a player_dict
+# with names for keys. If a new list with two players 
+# who have the same name is used, one of them will 
+# get overwritten. Because this script is written
+# specifically for the test file I'm leaving it
+# like that.
+
 if __name__ == "__main__":
     
     player_dict = {}
