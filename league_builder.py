f27ea0e37a71dd817b4d8cad4c37e52ccca4ed65
Alan W. Smith <alan@alanwsmith.com>
2016-12-27 20:04:49 -0500

    Getting the number of players with experience.

--- league_builder.py
+++ league_builder.py
@@ -4,21 +4,29 @@ import csv
 
 
 if __name__ == "__main__":
-	
-	players = []
+    
+    players = []
 
-	def load_players(): 
-	
-	    with open('soccer_players.csv', 'r') as csv_file:
-	        csv_data = csv.DictReader(csv_file)
-	        for row in csv_data:
-	        	players.append({
+    def load_players(): 
+        with open('soccer_players.csv', 'r') as csv_file:
+            csv_data = csv.DictReader(csv_file)
+            for row in csv_data:
+                players.append({
                     'name': row['Name'],
                     'height': row['Height (inches)'],
                     'experience': row['Soccer Experience'],
                     'guardian': row['Guardian Name(s)']
-	        	})
+                })
 
-	load_players()
-	print(players)
+    def players_with_experience():
+        count = 0
+        for player in players:
+        	if player['experience'] == 'YES':
+        		count += 1
+        return count
+
+
+    load_players()
+    print(players_with_experience())
+    print(players)
 
